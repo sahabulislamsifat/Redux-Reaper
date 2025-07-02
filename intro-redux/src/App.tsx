35827b94d102e1f980033320c796f45b4429e4de
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-07-03 01:28:59 +0600

    close

--- intro-redux/src/App.tsx
+++ intro-redux/src/App.tsx
@@ -1,11 +1,29 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/features/hook/hook";
+
 function App() {
+  const dispatch = useAppDispatch();
+
+  const { count } = useAppSelector((state) => state.reducer);
+  console.log(count);
+
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <>
       <div>
         <h1>Counter with Redux</h1>
-        <button>Increment</button>
-        <div>0</div>
-        <button>Decrement</button>
+        <button onClick={() => handleIncrement(5)}>Increment by 5</button>
+        <button onClick={() => handleIncrement(1)}>Increment</button>
+        <div>{count}</div>
+        {/* <div>0</div> */}
+        <button onClick={handleDecrement}>Decrement</button>
       </div>
     </>
   );
